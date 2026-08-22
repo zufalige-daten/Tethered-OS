8d0d780c1a058b6ac3f49d2e804dcf6e97d1b653
zufalige-daten <vestauk01@gmail.com>
2026-08-22 16:58:05 +0100

    :3c

--- kernel/src/arch/arch_setup.c
+++ kernel/src/arch/arch_setup.c
@@ -23,73 +23,72 @@ extern uint64_t kernel_pml4_map[512]; // the kernel's main pml4 map root map
 extern uint64_t kernel_pml3_map_n[256*512]; // id map 1 GiB pages
 
 void set_tss_des(long_system_segment_descriptor_t *tss_des, uint64_t base, uint64_t limit, uint8_t access_byte, uint8_t flags){
-	tss_des->access_byte = access_byte;
-	tss_des->limit1 = limit & 0xffff;
-	tss_des->flaglimit2 = ((limit & 0x0000f) << 16) | (flags << 4);
-	tss_des->base1 = base & 0xffff;
-	tss_des->base2 = (base & 0x0000ff) << 16;
-	tss_des->base3 = (base & 0x000000ff) << 24;
-	tss_des->base4 = (base & 0x00000000ffffffff) << 32;
-	tss_des->reserved = 0;
+  tss_des->access_byte = access_byte;
+  tss_des->limit1 = limit & 0xffff;
+  tss_des->flaglimit2 = ((limit & 0x0000f) << 16) | (flags << 4);
+  tss_des->base1 = base & 0xffff;
+  tss_des->base2 = (base & 0x0000ff) << 16;
+  tss_des->base3 = (base & 0x000000ff) << 24;
+  tss_des->base4 = (base & 0x00000000ffffffff) << 32;
+  tss_des->reserved = 0;
 }
 
 void arch_setup(kernel_header_struct_t kheader){
-	kernel_printf("Setting up paging... ");
-	// set up pat
-	uint64_t pat__ = kernel_pat;
-	uint32_t *pat_ptr__ = (uint32_t *)(&pat__);
-	setmsr(0x277, pat_ptr__[0], pat_ptr__[1]);
-	// note before anything
-	// identity map the first 16 TiB of memory
-	// clear the pml4 and pml3 tables
-	// allocate all of the specific addresses
-	for(uint64_t i = 0; i < 16 * 2; i++){
-		kernel_pml4_map[i] = ((uint64_t)(&kernel_pml3_map_n[i * 512])) | PML_P | PML_RW | PML_US;
-	}
-	for(uint64_t i = 0; i < 16 * 2 * 512; i++){
-		kernel_pml3_map_n[i] = (i * (1024*1024*1024)) | PML_P | PML_RW | PML_PS | PML_US;
-	}
-	asm volatile(
-		"mov cr3, rax\n"
-		::
+  kernel_printf("Setting up paging... ");
+  // set up pat
+  uint64_t pat__ = kernel_pat;
+  uint32_t *pat_ptr__ = (uint32_t *)(&pat__);
+  setmsr(0x277, pat_ptr__[0], pat_ptr__[1]);
+  // note before anything
+  // identity map the first 16 TiB of memory
+  // clear the pml4 and pml3 tables
+  // allocate all of the specific addresses
+  for(uint64_t i = 0; i < 16 * 2; i++){
+    kernel_pml4_map[i] = ((uint64_t)(&kernel_pml3_map_n[i * 512])) | PML_P | PML_RW | PML_US;
+  }
+  for(uint64_t i = 0; i < 16 * 2 * 512; i++){
+    kernel_pml3_map_n[i] = (i * (1024*1024*1024)) | PML_P | PML_RW | PML_PS | PML_US;
+  }
+  asm volatile(
+	       "mov cr3, rax\n"
+	       ::
 		"a" ((uint64_t)(&kernel_pml4_map[0]))
-	);
-	kernel_printf("[SUCCESS]\n");
-	kernel_printf("Setting up ACPI... ");
-	acpi_init();
-	kernel_printf("[SUCCESS]\n");
-	kernel_printf("Getting CPU Core Count... ");
-	acpi_walk_madt();
+	       );
+  kernel_printf("[SUCCESS]\n");
+  kernel_printf("Setting up ACPI... ");
+  acpi_init();
+  kernel_printf("[SUCCESS]\n");
+  kernel_printf("Getting CPU Core Count... ");
+  acpi_walk_madt();
 #ifndef TSS_MONO
-	tss_count = cpucorecount;
+  tss_count = cpucorecount;
 #endif
 #ifdef TSS_MONO
-	tss_count = 1;
+  tss_count = 1;
 #endif
-	kernel_printf("[SUCCESS]\n");
-	kernel_printf("Setting up new GDT... ");
-	gdtr.gdt = &gdt;
-	gdtr.size = ((5*8)+(tss_count*16)) - 1;
-	gdt.null = 0;
-	gdt.k_code.base1 = 0;
-	gdt.k_code.base2 = 0;
-	gdt.k_code.base3 = 0;
-	gdt.k_code.limit1 = 0xffff;
-	gdt.k_code.flaglimit2 = 0b10101111;
-	gdt.k_code.access_byte = 0b10011010;
-	gdt.u_code = gdt.k_code;
-	gdt.u_code.access_byte = 0b11111010;
-	gdt.k_data = gdt.k_code;
-	gdt.k_data.flaglimit2 = 0b11001111;
-	gdt.k_data.access_byte = 0b10010010;
-	gdt.u_data = gdt.k_data;
-	gdt.u_data.access_byte = 0b11110010;
-	for(uint64_t i = 0; i < tss_count; i++){
-		set_tss_des((long_system_segment_descriptor_t *)(&gdt.tss[i]), (uint64_t)(&tss[i]), sizeof(tss_t) - 1, 0b11101001, 0x0);
-		tss[i].iopb = sizeof(tss_t);
-		tss[i].rsp0 = ((uint64_t)&stack_top) - (65536 * (i + 1));
-	}
-	reload_gdt(&gdtr);
-	kernel_printf("[SUCCESS]\n");
+  kernel_printf("[SUCCESS]\n");
+  kernel_printf("Setting up new GDT... ");
+  gdtr.gdt = &gdt;
+  gdtr.size = ((5*8)+(tss_count*16)) - 1;
+  gdt.null = 0;
+  gdt.k_code.base1 = 0;
+  gdt.k_code.base2 = 0;
+  gdt.k_code.base3 = 0;
+  gdt.k_code.limit1 = 0xffff;
+  gdt.k_code.flaglimit2 = 0b10101111;
+  gdt.k_code.access_byte = 0b10011010;
+  gdt.u_code = gdt.k_code;
+  gdt.u_code.access_byte = 0b11111010;
+  gdt.k_data = gdt.k_code;
+  gdt.k_data.flaglimit2 = 0b11001111;
+  gdt.k_data.access_byte = 0b10010010;
+  gdt.u_data = gdt.k_data;
+  gdt.u_data.access_byte = 0b11110010;
+  for(uint64_t i = 0; i < tss_count; i++){
+    set_tss_des((long_system_segment_descriptor_t *)(&gdt.tss[i]), (uint64_t)(&tss[i]), sizeof(tss_t) - 1, 0b11101001, 0x0);
+    tss[i].iopb = sizeof(tss_t);
+    tss[i].rsp0 = ((uint64_t)&stack_top) - (65536 * (i + 1));
+  }
+  reload_gdt(&gdtr);
+  kernel_printf("[SUCCESS]\n");
 }
-
--- kernel/src/kernel.c
+++ kernel/src/kernel.c
@@ -19,43 +19,42 @@ extern void *bss_end;
 #include <arch/tasksw.h>
 
 void kernel_main(void){
-	kernel_printf("Successefully booted into kernel (x86_64).\n");
-	kernel_header_struct_t volatile kheader = *khs;
-	arch_setup(kheader);
-	size_t lfb_ptr = kheader.lfb;
-	// note before memory allocation
-	// programs are linked such that they are flat binaries with their origin of execution at 128 TiB because before that is the kernel identity map region where it is one large kernel level data segment with the entire identity map and that when a program is to be executed the only thing that needs to be done is to load their local pml4 (full table where lower 128 TiB is identity mapped / reserved as kernel data that is executable) as entry 2 in the pml4 table to then have it's execution continued
-	fast_memset_q(lfb_ptr, (kheader.lfb_height*kheader.lfb_width)/2, 0x0000000000000000);
-	kernel_printf("kheader.mem_size = %u MiB.\n", (kheader.mem_size / (1024 * 1024)));
-	kernel_printf("LFB addr = 0x%x.\n", kheader.lfb);
-	// extra avoidences
-	boot_mmap_entry_t mmdevs_region; // note: starts at 0xc0000000
-	mmdevs_region.baseaddr = 0xc0000000;
-	mmdevs_region.size = 1024*1024*1024;
-	kernel_printf("Setting up physical memory allocator... ");
-	init_pmemalloc();
-	kernel_printf("pmem_map_ptr = 0x%x.\n", pmem_map_ptr);
-	kernel_printf("pmem_map_size = %u.\n", pmem_map_size);
-	kernel_printf("pmem_max = %u.\n", pmem_max);
-	// test_pmemalloc();
-	physalloc(0, (uint64_t)&kernel_end);
-	physalloc((uint64_t)&bss_start, (uint64_t)&bss_end - (uint64_t)&bss_start);
-	physalloc(mmdevs_region.baseaddr, mmdevs_region.size);
-	physalloc(lfb_ptr, (kheader.lfb_width * kheader.lfb_height) * 4);
-	physalloc(0xa0000, 0xbffff-0xa0000);
-	physalloc(pmem_map_ptr, pmem_map_size);
-	kernel_printf("[SUCCESS]\n");
-	uint64_t continguent = getlowestfree(0);
-	pmem_lowest_free = continguent;
-	// interrupts :: int 0x80 : system call to procedure provided by the system.
-	kernel_printf("Setting up interrupts... ");
-	interrupts_init();
-	interrupts_register_all();
-	kernel_printf("[SUCCESS]\n");
-	asm volatile("int 0x80" :: "b"(8L));
-	asm volatile("int 0x80" :: "b"(0L));
-	// next TODO :: attempt to switch to a usermode piece of code after setting up the tss, implement task switching.
-	test_usermode();
-	while(1);
+  kernel_printf("Successefully booted into kernel (x86_64).\n");
+  kernel_header_struct_t volatile kheader = *khs;
+  arch_setup(kheader);
+  size_t lfb_ptr = kheader.lfb;
+  // note before memory allocation
+  // programs are linked such that they are flat binaries with their origin of execution at 128 TiB because before that is the kernel identity map region where it is one large kernel level data segment with the entire identity map and that when a program is to be executed the only thing that needs to be done is to load their local pml4 (full table where lower 128 TiB is identity mapped / reserved as kernel data that is executable) as entry 2 in the pml4 table to then have it's execution continued
+  fast_memset_q(lfb_ptr, (kheader.lfb_height*kheader.lfb_width)/2, 0x0000000000000000);
+  kernel_printf("kheader.mem_size = %u MiB.\n", (kheader.mem_size / (1024 * 1024)));
+  kernel_printf("LFB addr = 0x%x.\n", kheader.lfb);
+  // extra avoidences
+  boot_mmap_entry_t mmdevs_region; // note: starts at 0xc0000000
+  mmdevs_region.baseaddr = 0xc0000000;
+  mmdevs_region.size = 1024*1024*1024;
+  kernel_printf("Setting up physical memory allocator... ");
+  init_pmemalloc();
+  kernel_printf("pmem_map_ptr = 0x%x.\n", pmem_map_ptr);
+  kernel_printf("pmem_map_size = %u.\n", pmem_map_size);
+  kernel_printf("pmem_max = %u.\n", pmem_max);
+  // test_pmemalloc();
+  physalloc(0, (uint64_t)&kernel_end);
+  physalloc((uint64_t)&bss_start, (uint64_t)&bss_end - (uint64_t)&bss_start);
+  physalloc(mmdevs_region.baseaddr, mmdevs_region.size);
+  physalloc(lfb_ptr, (kheader.lfb_width * kheader.lfb_height) * 4);
+  physalloc(0xa0000, 0xbffff-0xa0000);
+  physalloc(pmem_map_ptr, pmem_map_size);
+  kernel_printf("[SUCCESS]\n");
+  uint64_t continguent = getlowestfree(0);
+  pmem_lowest_free = continguent;
+  // interrupts :: int 0x80 : system call to procedure provided by the system.
+  kernel_printf("Setting up interrupts... ");
+  interrupts_init();
+  interrupts_register_all();
+  kernel_printf("[SUCCESS]\n");
+  asm volatile("int 0x80" :: "b"(8L));
+  asm volatile("int 0x80" :: "b"(0L));
+  // next TODO :: attempt to switch to a usermode piece of code after setting up the tss, implement task switching.
+  test_usermode();
+  while(1);
 }
-
